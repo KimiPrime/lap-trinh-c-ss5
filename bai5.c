f12ca8ad1a9f7ec4ddbf8503ac116399997411cb
KimiPrime <kingsan049@gmail.com>
2024-11-26 01:04:05 +0700

    Add files via upload

---
+++ bai5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	int i;
+    for(n = 1;n <= 9; n++) {
+        printf("Bang cuu chuong cua %d:\n", n);
+        for(i = 1; i <= 10; i++) {
+            printf("%d x %d = %d\n", n, i, n * i);
+        }
+    }
+    return 0;
+}
